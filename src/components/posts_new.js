35fad332091aa8323ea15bb830cd0e92958b1104
Sillas Camara <sillasmc@gmail.com>
2018-05-09 21:03:36 -0300

    Show Form in New post route

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import {  Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	renderField (field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input
+					className="form-control"
+					{...field.input} // Wire the event handlers of Field to input
+				/>
+			</div>
+		);
+	}
+
+	renderTextAreaField (field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<textarea className="form-control" rows="6" {...field.input}></textarea>
+			</div>
+		);
+	}
+
 	render () {
 		return (
 			<div>
-				Posts New
+				<h2>Posts New</h2>
+				<form>
+					<Field
+						label="Title"
+						name="title"
+						component={this.renderField}
+					/>
+					<Field
+						label="Tags"
+						name="tags"
+						component={this.renderField}
+					/>
+					<Field
+						label="Post Content"
+						name="content"
+						component={this.renderTextAreaField}
+					/>
+				</form>
 			</div>
 		);
 	}
 }
 
-export default PostsNew;
+export default reduxForm({
+	form: 'PostsNewForm' // Identifies the form
+})(PostsNew);

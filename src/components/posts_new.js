1a062e7a09e80620195c4e89c3b7c816cc6cbb71
Sillas Camara <sillasmc@gmail.com>
2018-05-11 22:29:46 -0300

    Create cancel form button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {  Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 	renderField (field) {
@@ -63,7 +64,7 @@ class PostsNew extends Component {
 						component={this.renderTextAreaField}
 					/>
 					<button type="submit" className="btn btn-primary">Save</button>
-					{/* <button className="btn btn-danger">Cancel</button> */}
+					<Link className="btn btn-danger" to="/">Cancel</Link>
 				</form>
 			</div>
 		);
